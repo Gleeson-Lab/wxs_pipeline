9c43acf2984dbbe330898490d6f94765aad4c2f4
Brett Copeland <brcopeland@gmail.com>
2021-08-19 15:14:08 -0700

    Refactored getting parameters for rules.

---
+++ {{cookiecutter.project_name}}/workflow/utilities.py
@@ -0,0 +1,19 @@
+from itertools import chain
+
+def get_options(rule_name, prefix="--", separator=" ", extra=[],
+                ignore=set()):
+    """
+    Return extra parameters for the specified rule defined in the config file.
+    :param rule_name: used to look in the appropriate section of the config file
+    :param prefix: the string to prepend to parameters
+    :param separator: the string to separate the parameter from its value
+    :param extra: an iterable of (parameter, value) entries to additionally include
+    :param ignore: a set of parameters to ignore
+    """
+    parameters = []
+    for parameter, values in chain(config[rule_name].items(), extra):
+        if type(values) is not list:
+            values = [values]
+        for value in values:
+            parameters.append(f"{prefix}{parameter}{separator}{value}")
+    return " ".join(parameters)
