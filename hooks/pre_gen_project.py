4b892e4efb446fdcb90c7028d31c2520375e98bc
Brett Copeland <brcopeland@gmail.com>
2021-08-24 11:09:26 -0700

    Implemented 1. running on GRCh37 and 2. prompting user for the genome build to choose.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,3 +4,12 @@ project_name = "{{cookiecutter.project_name}}"
 if " " in project_name:
     print("project_name is used as the name of the directory, so should not have any spaces")
     sys.exit(1)
+
+genome_build = "{{cookiecutter.genome_build.lower()}}"
+import yaml
+with open("../{{cookiecutter.project_name}}/config/config.yaml") as c:
+    config = yaml.load(c, yaml.Loader)
+if genome_build not in config["reference"]:
+    print(f"genome_build ({genome_build}) not found in config.yaml.  "
+          f"Valid options are {','.join(config['reference'].keys())}")
+    sys.exit(1)
