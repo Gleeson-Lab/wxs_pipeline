d2315a769f95f94fe89e3f2f1d64037e4c7d240e
Brett Copeland <brcopeland@gmail.com>
2021-08-24 11:24:12 -0700

    Moved genome_build error checking to post_gen_project.py.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,15 @@
 import os
+import yaml
+
+genome_build = "{{cookiecutter.genome_build.lower()}}"
+with open("config/config.yaml") as c:
+    config = yaml.load(c, yaml.Loader)
+if genome_build not in config["reference"]:
+    print(f"genome_build ({genome_build}) not found in config.yaml.  "
+          f"Valid options are {','.join(config['reference'].keys())}")
+    sys.exit(1)
 
 user = os.getlogin()
-
 logs = "{{cookiecutter.logs}}".replace("$USER", user)
 output = "{{cookiecutter.output}}".replace("$USER", user)
 resources = "{{cookiecutter.resources}}".replace("$USER", user)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,12 +4,3 @@ project_name = "{{cookiecutter.project_name}}"
 if " " in project_name:
     print("project_name is used as the name of the directory, so should not have any spaces")
     sys.exit(1)
-
-genome_build = "{{cookiecutter.genome_build.lower()}}"
-import yaml
-with open("config/config.yaml") as c:
-    config = yaml.load(c, yaml.Loader)
-if genome_build not in config["reference"]:
-    print(f"genome_build ({genome_build}) not found in config.yaml.  "
-          f"Valid options are {','.join(config['reference'].keys())}")
-    sys.exit(1)
