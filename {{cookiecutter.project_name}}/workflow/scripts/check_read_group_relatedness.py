49e1b88034144db0462504a53a7c5c969d1c5519
Brett Copeland <brcopeland@gmail.com>
2021-10-14 11:49:05 -0700

    1. Disabled group job submission.  There appears to be some sort of    internal bug in snakemake pertaining to group jobs. 2. Implemented alternate read group relatedness test in    check_read_group_relatedness.py in the case of haploid data.

--- {{cookiecutter.project_name}}/workflow/scripts/check_read_group_relatedness.py
+++ {{cookiecutter.project_name}}/workflow/scripts/check_read_group_relatedness.py
@@ -10,7 +10,8 @@ RELATEDNESS_FAILURE_MESSAGE = (
     f"Failed read group relatedness check on {snakemake.wildcards.sample}."
 )
 # checked so as to not email more than once
-already_failed =  os.path.isfile(snakemake.params.failure_file)
+already_failed = os.path.isfile(snakemake.params.failure_file)
+haploid = snakemake.params.haploid
 if snakemake.log:
     log_fh = open(str(snakemake.log), "w")
 else:
@@ -19,7 +20,10 @@ else:
     log_fh = sys.stdout
 try:
     rg_pairs_with_relatedness_problem = []
-    log_lines = ["read_group_a\tread_group_b\trelatedness\tflag"]
+    if haploid:
+        log_lines = ["read_group_a\tread_group_b\tibs0\tibs2"]
+    else:
+        log_lines = ["read_group_a\tread_group_b\trelatedness\tflag"]
     comparisons = {}
     with open(snakemake.input[0]) as somalier_fh:
         header = somalier_fh.readline().strip().split("\t")
@@ -30,18 +34,34 @@ try:
             b = int(d["sample_b"].split("_")[0])
             if b < a:
                 a, b = b, a
-            relatedness = float(d["relatedness"])
-            comparisons[(a, b)] = relatedness
+            if haploid:
+                value = (int(d["ibs0"]), int(d["ibs2"]))
+            else:
+                value = float(d["relatedness"])
+            comparisons[(a, b)] = value
     for read_group_a, read_group_b in sorted(comparisons):
-        relatedness = comparisons[(read_group_a, read_group_b)]
-        if relatedness < snakemake.params.min_relatedness:
-            rg_pairs_with_relatedness_problem.append(
-                f"{read_group_a},{read_group_b}"
-            )
-            flag = "error"
+        if haploid:
+            ibs0, ibs2 = comparisons[(read_group_a, read_group_b)]
+            if ibs0 or not ibs2:
+                # failure if any marker has ibs0 or if there are none with ibs2
+                rg_pairs_with_relatedness_problem.append(
+                    f"{read_group_a},{read_group_b}"
+                )
+                flag = "error"
+            else:
+                flag = "ok"
+            value = f"{ibs0}\t{ibs2}"
         else:
-            flag = "ok"
-        log_lines.append(f"{read_group_a}\t{read_group_b}\t{relatedness}\t{flag}")
+            relatedness = comparisons[(read_group_a, read_group_b)]
+            if relatedness < snakemake.params.min_relatedness:
+                rg_pairs_with_relatedness_problem.append(
+                    f"{read_group_a},{read_group_b}"
+                )
+                flag = "error"
+            else:
+                flag = "ok"
+            value = relatedness
+        log_lines.append(f"{read_group_a}\t{read_group_b}\t{value}\t{flag}")
     log_fh.write("\n".join(log_lines) + "\n")
 finally:
     if snakemake.log:
@@ -50,9 +70,12 @@ finally:
 if rg_pairs_with_relatedness_problem:
     message = (
         f"The following read group pairs of {snakemake.wildcards.sample}: "
-        f"{'; '.join(rg_pairs_with_relatedness_problem)} had less than "
-        f"{snakemake.params.min_relatedness} relatedness.\n"
+        f"{'; '.join(rg_pairs_with_relatedness_problem)}"
     )
+    if haploid:
+        message += " had a relatedness failure.\n"
+    else:
+        message += f"had less than {snakemake.params.min_relatedness} relatedness.\n"
     with open(snakemake.params.failure_file, "w") as failure_fh:
         failure_fh.write(message)
     if not already_failed:
